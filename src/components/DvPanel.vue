d4ae59d879580e15f277c95f48ad34b21cc16649
Philip Baker <philip.baker21@gmail.com>
2020-12-18 11:23:03 -0500

    DvPanel: basic panel with responsive about display

---
+++ src/components/DvPanel.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="rounded overflow-hidden border border-black">
+    <div
+      class="px-2 py-1 border-bottom border-black bg-indBlue text-white text-lg"
+    >
+      Panel Title
+    </div>
+    <div class="p-2 bg-gray-200">
+      <div class="h-16">
+        Content
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+// @import 'dv/lib/SASS/_vars.scss';
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div
+    class="p-4 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2"
+  >
+    <DvPanel />
+    <DvPanel />
+    <DvPanel />
+    <DvPanel />
+    <DvPanel />
+    <DvPanel />
   </div>
 </template>
+
+<script>
+import DvPanel from "@/components/DvPanel.vue";
+export default {
+  components: {
+    DvPanel
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
